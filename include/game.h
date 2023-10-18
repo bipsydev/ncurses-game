f88f2c62edab38c3e2a8ed9e5e73a9c292a1036e
bipsydev <bipsydev@gmail.com>
2023-10-18 13:44:02 -0500

    Break up run loop into private methods

--- include/game.h
+++ include/game.h
@@ -7,10 +7,26 @@ namespace LCode
 
 class Game
 {
+    char key;
+    int cursor_y, cursor_x, screen_y, screen_x;
+    unsigned short count;
+    bool running, ran;
+
 public:
     Game();
     int run();
     ~Game();
+
+private:
+    // These are ran once at the beginning and ending of run()
+    void run_begin();
+    void run_end();
+
+    // These are part of the game run loop
+    void events();      // gather input & game events
+    void update();      // update game logic 1 tick of time, responding to events
+    void draw();        // draw to the screen
+    void post_update(); // prepare data for next frame update after draw
 };
 
 } // namespace LCode
--- src/game.cpp
+++ src/game.cpp
@@ -5,60 +5,106 @@
 #include <string>   // std::string, std::to_string
 #include <cstdlib>  // EXIT_SUCCESS
 
-LCode::Game::Game()
+namespace LCode
 {
+
+Game::Game()
+: key{'\0'},
+  cursor_y{-1}, cursor_x{-1},
+  screen_y{-1}, screen_x{-1},
+  count{0},
+  running{false}, ran{false}
+{
+    // set the locale (allows Unicode)
+    setlocale(LC_ALL, "");
     // initialize screen
     initscr();
     // disable TTY driver buffer of typed characters (get per character)
     cbreak();
     // disable echoing of typed characters
     noecho();
+    // flushes the screen when getting interrupt signal (faster?)
+    intrflush(stdscr, FALSE);
     // enable backspace, delete, and arrow keys
     keypad(stdscr, TRUE);
 }
 
-int LCode::Game::run()
+int Game::run()
+{
+    run_begin();
+    while (running)
+    {
+        events();
+        update();
+        draw();
+        post_update();
+    }
+    run_end();
+
+    return EXIT_SUCCESS;
+}
+
+Game::~Game()
+{
+    // deallocates the screen
+    endwin();
+}
+
+/******************************************************************************
+ *                             PRIVATE METHODS                                *
+ ******************************************************************************/
+
+void Game::run_begin()
 {
     //print to the screen
     printw("Hello World!\n");
     refresh();
 
-    char key;
-    int cursor_y, cursor_x, screen_y, screen_x;
-    unsigned short count = 0;
-    do
-    {
-        getyx(stdscr, cursor_y, cursor_x);
-        getmaxyx(stdscr, screen_y, screen_x);
-        // wait for user input
-        key = static_cast<char>(getch());
-        if (cursor_y >= screen_y - 1 && cursor_x > 0)
-        {
-            clear();
-            cursor_y = 0;
-        }
-        std::string message = "You pressed \"" + std::string{key} + "\" ("
-                            + std::to_string(static_cast<int>(key)) + ") !";
-        printw(message.c_str());
-
-        printw((" line = " + std::to_string(cursor_y)).c_str());
-        printw((" / " + std::to_string(screen_y - 1)).c_str());
-
-        printw("\n");
-        if (count == 5)
-        {
-            printw("Press Backspace when you want to exit the program.\n");
-        }
-        refresh();
-        ++count;
-    }
-    while (key != '\b' /* backspace */);
-
-    return EXIT_SUCCESS;
+    count = 0;
+    running = true;
 }
 
-LCode::Game::~Game()
+void Game::run_end()
+{ }
+
+void Game::events()
 {
-    // deallocates the screen
-    endwin();
+    getyx(stdscr, cursor_y, cursor_x);
+    getmaxyx(stdscr, screen_y, screen_x);
+    // wait for user input
+    key = static_cast<char>(getch());
 }
+
+void Game::update()
+{
+    if (cursor_y >= screen_y - 1 && cursor_x > 0)
+    {
+        clear();
+        cursor_y = 0;
+    }
+}
+
+void Game::draw()
+{
+    std::string message = "You pressed \"" + std::string{key} + "\" ("
+                        + std::to_string(static_cast<int>(key)) + ") !";
+    printw(message.c_str());
+
+    printw((" line = " + std::to_string(cursor_y)).c_str());
+    printw((" / " + std::to_string(screen_y - 1)).c_str());
+
+    printw("\n");
+    if (count == 5)
+    {
+        printw("Press Backspace when you want to exit the program.\n");
+    }
+    refresh();
+}
+
+void Game::post_update()
+{
+    ++count;
+    running = key != '\b';
+}
+
+} // namespace LCode
